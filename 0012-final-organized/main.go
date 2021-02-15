6f8e6a3975745080628914e9e9bf6322d2732bd5
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2021-02-15 21:42:26 +0000

    organize with go modules

---
+++ 0012-final-organized/main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type user struct {
+	password    []byte
+	displayName string
+}
+
+var database = map[string]user{}
+var sessions = map[string]string{}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/logout", logoutHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("session")
+	if err != nil {
+		c = &http.Cookie{
+			Name:  "session",
+			Value: "",
+		}
+	}
+
+	sid, err := parseToken(c.Value)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	name := ""
+	if sid != "" {
+		email := sessions[sid]
+		name = database[email].displayName
+	}
+
+	html := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>HMAC Example (Part 1)</title>
+	</head>
+	<body>
+		<h3> register </h3>
+		<form action="/register" method="post">
+			<input type="text" name="name" placeholder="Display Name" />
+			<input type="email" name="email" />
+			<input type="password" name="password" />
+			<input type="submit" />
+		</form>
+		<h3> login </h3>
+		<form action="/login" method="post">
+			<input type="email" name="email" />
+			<input type="password" name="password" />
+			<input type="submit" />
+		</form>
+	</body>
+	</html>`
+
+	if name != "" {
+		html = `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta http-equiv="X-UA-Compatible" content="ie=edge">
+			<title>HMAC Example (Part 1)</title>
+		</head>
+		<body>
+		<h3> Welcome back ` + name + `</h3>
+		<form action="/logout" method="post">
+			<input type="submit" value="logout" />
+		</form>
+		</body>
+		</html>`
+	}
+
+	io.WriteString(w, html)
+}
+
+func registerHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	email := req.FormValue("email")
+	name := req.FormValue("name")
+	password := req.FormValue("password")
+	if email == "" || password == "" || name == "" {
+		http.Redirect(w, req, "/", http.StatusBadRequest)
+		return
+	}
+
+	b, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+	database[email] = user{
+		displayName: name,
+		password:    b,
+	}
+
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+func loginHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	email := req.FormValue("email")
+	password := req.FormValue("password")
+	if email == "" || password == "" {
+		http.Redirect(w, req, "/", http.StatusBadRequest)
+		return
+	}
+
+	user, ok := database[email]
+	if !ok {
+		http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword(user.password, []byte(password))
+	if err != nil {
+		http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+		return
+	}
+
+	sUUID := uuid.New().String()
+	sessions[sUUID] = email
+	token, err := createToken(sUUID)
+	if err != nil {
+		//should log error
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	c := http.Cookie{
+		Name:  "session",
+		Value: token,
+	}
+	http.SetCookie(w, &c)
+
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+func logoutHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	c, err := req.Cookie("session")
+	if err != nil {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	sid, err := parseToken(c.Value)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	delete(sessions, sid)
+
+	c.MaxAge = -1 //expire
+	http.SetCookie(w, c)
+
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
