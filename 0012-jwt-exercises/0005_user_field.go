14cf8f1652eade75580c6a53abc8b91d6b6bee73
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2021-02-15 20:05:12 +0000

    Add user field to register and display

---
+++ 0012-jwt-exercises/0005_user_field.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var key = []byte("my croton has a bunch of new leaves :)")
+
+type user struct {
+	password    []byte
+	displayName string
+}
+
+var database = map[string]user{}
+var sessions = map[string]string{}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("session")
+	if err != nil {
+		c = &http.Cookie{
+			Name:  "session",
+			Value: "",
+		}
+	}
+
+	sid, err := parseToken(c.Value)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	name := ""
+	if sid != "" {
+		email := sessions[sid]
+		name = database[email].displayName
+	}
+
+	html := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>HMAC Example (Part 1)</title>
+	</head>
+	<body>
+		<h3> register </h3>
+		<form action="/register" method="post">
+			<input type="text" name="name" placeholder="Display Name" />
+			<input type="email" name="email" />
+			<input type="password" name="password" />
+			<input type="submit" />
+		</form>
+		<h3> login </h3>
+		<form action="/login" method="post">
+			<input type="email" name="email" />
+			<input type="password" name="password" />
+			<input type="submit" />
+		</form>
+	</body>
+	</html>`
+
+	if name != "" {
+		html = `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta http-equiv="X-UA-Compatible" content="ie=edge">
+			<title>HMAC Example (Part 1)</title>
+		</head>
+		<body>
+		<h3> Welcome back ` + name + `</h3>
+		</body>
+		</html>`
+	}
+
+	io.WriteString(w, html)
+}
+
+func registerHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	email := req.FormValue("email")
+	name := req.FormValue("name")
+	password := req.FormValue("password")
+	if email == "" || password == "" || name == "" {
+		http.Redirect(w, req, "/", http.StatusBadRequest)
+		return
+	}
+
+	b, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+	database[email] = user{
+		displayName: name,
+		password:    b,
+	}
+
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+func loginHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	email := req.FormValue("email")
+	password := req.FormValue("password")
+	if email == "" || password == "" {
+		http.Redirect(w, req, "/", http.StatusBadRequest)
+		return
+	}
+
+	user, ok := database[email]
+	if !ok {
+		http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword(user.password, []byte(password))
+	if err != nil {
+		http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+		return
+	}
+
+	sUUID := uuid.New().String()
+	sessions[sUUID] = email
+	token := createToken(sUUID)
+	c := http.Cookie{
+		Name:  "session",
+		Value: token,
+	}
+	http.SetCookie(w, &c)
+
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+// createToken receives session id and returns an hmac signed token encoded in b64
+func createToken(sid string) string {
+	mac := hmac.New(sha256.New, key)
+	mac.Write([]byte(sid))
+	//signedMac := fmt.Sprintf("%x", mac.Sum(nil)) //hex
+	signedMac := base64.StdEncoding.EncodeToString(mac.Sum(nil)) //base64
+
+	return signedMac + "|" + sid
+}
+
+// parseToken gets signed string and returns session id
+func parseToken(signed string) (string, error) {
+	xs := strings.SplitN(signed, "|", 2)
+	if len(xs) != 2 {
+		return "", fmt.Errorf("NOT VALID")
+	}
+
+	b64 := xs[0]
+	decodedToken, err := base64.StdEncoding.DecodeString(b64)
+	if err != nil {
+		return "", err
+	}
+
+	mac := hmac.New(sha256.New, key)
+	mac.Write([]byte(xs[1]))
+
+	if !hmac.Equal(decodedToken, mac.Sum(nil)) {
+		return "", fmt.Errorf("NOT VALID")
+	}
+
+	return xs[1], nil
+}
